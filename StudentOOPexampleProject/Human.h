187eaad63aded836654882584b73a0ba661be167
ITSTEP\st <st>
2024-05-07 18:37:15 +0300

    Add project files.

---
+++ StudentOOPexampleProject/Human.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Human
+{
+protected:
+	string name;
+	int age;
+
+public:
+	Human() {
+		cout << "Human default constructor" << endl;
+	}
+
+	Human(string name, int age) :
+		name(name), age(age) {}
+
+	~Human() {
+		cout << "Human destructor" << endl;
+	}
+
+	string getName();
+	void setName(string name);
+	int getAge();
+	void setAge(int age);
+
+	int sum(double a, double b);
+	int sum(int a, int b);
+	int sum(int a, int b, int c);
+
+	Human& operator+(int value) {
+		Human h(name, age + value);
+		return h;
+	}
+
+	Human& operator+(Human& human) {
+		Human h(name + "-" + human.name,
+			age + human.age);
+		return h;
+	}
+
+	string getInfo();
+};
